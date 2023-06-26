08e0286c8fdd425645f523fbca721330da2766dd
Ethan Orlander <ethanorlander@gmail.com>
2023-06-26 19:48:16 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 # Install errorai from source
 echo "Installing errorai from source..."
+brew update
 brew install ethanorlander/errorai/errorai
 
 # Add to ~/.zshrc
 echo "Adding to ~/.zshrc..."
 echo "" >> ~/.zshrc
 echo "# ErrorAI" >> ~/.zshrc
-echo 'export PATH="#{opt_prefix}/dist:$PATH"' >> ~/.zshrc
-echo '[[ -f \"#{opt_prefix}/shell/zshrc.zsh\" ]] && builtin source \"#{opt_prefix}/shell/zshrc.zsh\"' >> ~/.zshrc
+echo "export PATH=\"$(brew --prefix errorai)/dist:\$PATH\"" >> ~/.zshrc
+echo "[[ -f \"$(brew --prefix errorai)/shell/zshrc.zsh\" ]] && builtin source \"$(brew --prefix errorai)/shell/zshrc.zsh\"" >> ~/.zshrc
